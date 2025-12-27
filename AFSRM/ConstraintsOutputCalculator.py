6a0ee9a40e98b3d799dda8359e9f084ba3a3f890
JaineInvent <jaine.ratush@cooper.edu>
2025-12-27 03:18:52 -0500

    ok

---
+++ AFSRM/ConstraintsOutputCalculator.py
@@ -0,0 +1,11 @@
+import numpy as np
+''' 
+Calculations/inputs
+Resistivity of coils rho
+Resistance
+Coil spacing based on manufacturability(a)
+Back EMF(v_ind) 
+Final torque and power calculations
+'''
+
+v_supply = 5 #Volts 
---
+++ AFSRM/EngineeringLog.md
@@ -0,0 +1,3 @@
+## 12/27/25: 
+- Write a python script that uses/calculates electrical constraints to calculate max torque 
+- Generate PCB/ PCB coils 
